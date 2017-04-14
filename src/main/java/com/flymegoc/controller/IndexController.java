f7556b7db6a90c8773641256c3d3b9a6a8163cf1
ngl272 <343672271@qq.com>
2017-04-14 10:58:00 +0800

    change package jar to war

--- src/main/java/com/flymegoc/controller/IndexController.java
+++ src/main/java/com/flymegoc/controller/IndexController.java
@@ -12,6 +12,6 @@ public class IndexController {
 
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String indexPage(){
-        return "hello word444444";
+        return "hello springboot";
     }
 }
